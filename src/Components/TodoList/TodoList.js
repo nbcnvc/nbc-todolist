238d213a9488cdd97f50b931d1d52fcb81bb4c2a
nbcnvc <nbcnvc.dev@gmail.com>
2023-06-14 18:04:08 +0900

    Divide done / working todo list

--- src/Components/TodoList/TodoList.js
+++ src/Components/TodoList/TodoList.js
@@ -2,11 +2,33 @@ import Todo from "../Todo/Todo";
 import "./TodoList.css";
 
 const TodoList = (props) => {
+  const { doneList, workingList } = props.todos.reduce(
+    (acc, todo) => {
+      const component = (
+        <Todo
+          key={todo.id}
+          todo={todo}
+          onRemoveHandler={props.onRemoveTodoHandler}
+        />
+      );
+
+      if (todo.isDone) {
+        acc.doneList.push(component);
+      } else {
+        acc.workingList.push(component);
+      }
+      return acc;
+    },
+    { doneList: [], workingList: [] }
+  );
+
   return (
-    <div>
-      {props.todos.map((todo, idx) => (
-        <Todo key={idx} todo={todo} onRemoveHandler={props.onRemoveTodoHandler} />
-      ))}
+    <div className="todo-list">
+      <h2>working</h2>
+      <div className="todo-list__working">{workingList}</div>
+
+      <h2>done</h2>
+      <div className="todo-list__done">{doneList}</div>
     </div>
   );
 };
