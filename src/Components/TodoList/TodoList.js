c69ea8f240981cf30145794f0f40006695f021fe
nbcnvc <nbcnvc.dev@gmail.com>
2023-06-14 14:02:48 +0900

    Make TodoList

--- src/Components/TodoList/TodoList.js
+++ src/Components/TodoList/TodoList.js
@@ -1,7 +1,14 @@
-import './TodoList.css';
+import Todo from "../Todo/Todo";
+import "./TodoList.css";
 
-const TodoList = () => {
-  return
-}
+const TodoList = (props) => {
+  return (
+    <div>
+      {props.todos.map((todo, idx) => (
+        <Todo key={idx} todo={todo} />
+      ))}
+    </div>
+  );
+};
 
 export default TodoList;
